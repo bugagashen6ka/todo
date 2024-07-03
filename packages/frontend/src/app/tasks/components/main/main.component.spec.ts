4076f901893eac7b898f035f08573ef85dbb96d2
mila <lyudaburkan@gmail.com>
2024-07-03 20:04:42 +0200

    feat: adding npm workspaces

--- packages/frontend/src/app/tasks/components/main/main.component.spec.ts
+++ packages/frontend/src/app/tasks/components/main/main.component.spec.ts
@@ -66,27 +66,17 @@ describe('MainComponent', () => {
   it('initialises correctly with given tasks', () => {
     jest.spyOn(tasksService, 'getTasks').mockImplementation(() => {});
 
-    const showTitle = fixture.debugElement.query(
-      By.css(`[data-testid="showTitle"]`)
-    );
-    const showDescription = fixture.debugElement.query(
-      By.css(`[data-testid="showDescription"]`)
-    );
+    const showTitle = fixture.debugElement.query(By.css(`[data-testid="showTitle"]`));
+    const showDescription = fixture.debugElement.query(By.css(`[data-testid="showDescription"]`));
     const todo = fixture.debugElement.query(By.css(`[data-testid="todo"]`));
 
-    expect(showTitle.nativeElement.textContent.trim()).toEqual(
-      dummyTasks[0].title
-    );
-    expect(showDescription.nativeElement.textContent.trim()).toEqual(
-      dummyTasks[0].description
-    );
+    expect(showTitle.nativeElement.textContent.trim()).toEqual(dummyTasks[0].title);
+    expect(showDescription.nativeElement.textContent.trim()).toEqual(dummyTasks[0].description);
     expect(todo.classes['editing']).not.toBeDefined();
   });
 
   it('should activate editing mode and pass correct task id for title', () => {
-    const showTitle = fixture.debugElement.query(
-      By.css(`[data-testid="showTitle"]`)
-    );
+    const showTitle = fixture.debugElement.query(By.css(`[data-testid="showTitle"]`));
     showTitle.triggerEventHandler('click');
 
     expect(component.editingTaskId).toEqual(id);
@@ -94,9 +84,7 @@ describe('MainComponent', () => {
   });
 
   it('should activate editing mode and pass correct task id for description', () => {
-    const showDescription = fixture.debugElement.query(
-      By.css(`[data-testid="showDescription"]`)
-    );
+    const showDescription = fixture.debugElement.query(By.css(`[data-testid="showDescription"]`));
     showDescription.triggerEventHandler('click');
 
     expect(component.editingTaskId).toEqual(id);
@@ -108,19 +96,15 @@ describe('MainComponent', () => {
     jest.spyOn(tasksService, 'updateTask').mockImplementation(() => {});
     component.setInEditMode(dummyTasks[0].id, 'title');
     fixture.detectChanges();
-    const editTitle = fixture.debugElement.query(
-      By.css(`[data-testid="editTitle"]`)
-    );
+    const editTitle = fixture.debugElement.query(By.css(`[data-testid="editTitle"]`));
     editTitle.nativeElement.value = editedTitle;
-    editTitle.nativeElement.dispatchEvent(
-      new KeyboardEvent('keyup', { key: 'Enter' })
-    );
+    editTitle.nativeElement.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
 
     expect(tasksService.updateTask).toHaveBeenCalledWith(
       id,
       editedTitle,
       dummyTasks[0].description,
-      dummyTasks[0].completed
+      dummyTasks[0].completed,
     );
   });
 
@@ -129,44 +113,36 @@ describe('MainComponent', () => {
     jest.spyOn(tasksService, 'updateTask').mockImplementation(() => {});
     component.setInEditMode(dummyTasks[0].id, 'description');
     fixture.detectChanges();
-    const editDescription = fixture.debugElement.query(
-      By.css(`[data-testid="editDescription"]`)
-    );
+    const editDescription = fixture.debugElement.query(By.css(`[data-testid="editDescription"]`));
     editDescription.nativeElement.value = editedDescription;
-    editDescription.nativeElement.dispatchEvent(
-      new KeyboardEvent('keyup', { key: 'Enter' })
-    );
+    editDescription.nativeElement.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
 
     expect(tasksService.updateTask).toHaveBeenCalledWith(
       id,
       dummyTasks[0].title,
       editedDescription,
-      dummyTasks[0].completed
+      dummyTasks[0].completed,
     );
   });
 
   it('should change completion state of the task', () => {
     jest.spyOn(tasksService, 'updateTask').mockImplementation(() => {});
 
-    const toggleTask = fixture.debugElement.query(
-      By.css(`[data-testid="toggle"]`)
-    );
+    const toggleTask = fixture.debugElement.query(By.css(`[data-testid="toggle"]`));
     toggleTask.nativeElement.click();
 
     expect(tasksService.updateTask).toHaveBeenCalledWith(
       id,
       dummyTasks[0].title,
       dummyTasks[0].description,
-      !dummyTasks[0].completed
+      !dummyTasks[0].completed,
     );
   });
 
   it('should delete the task', () => {
     jest.spyOn(tasksService, 'deleteTask').mockImplementation(() => {});
 
-    const deleteButton = fixture.debugElement.query(
-      By.css(`[data-testid="deleteButton"]`)
-    );
+    const deleteButton = fixture.debugElement.query(By.css(`[data-testid="deleteTaskButton"]`));
     deleteButton.nativeElement.click();
 
     expect(tasksService.deleteTask).toHaveBeenCalledWith(id);
